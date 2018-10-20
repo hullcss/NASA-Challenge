348868b5563269b44c2b08f23534d190f5183a0d
Unknown <alex.denys.lovett@gmail.com>
2018-10-20 15:54:48 +0100

    Clean Code

--- SpaceApps/Models/CleanData.cs
+++ SpaceApps/Models/CleanData.cs
@@ -13,23 +13,65 @@ namespace SpaceApps.Models.CleanData
         Success,
         Fail,
     };
-    public class Launch
+
+    public class HarrysClass
     {
-        public int Id  {get; set;}
+        public int Id { get; set; }
         public int RocketFamilyName { get; set; }
         public double Latitude { get; set; }
-        public double Longitude { get; set;}
-        public string LaunchPadName {get; set;}
+        public double Longitude { get; set; }
+        public string LaunchPadName { get; set; }
         public string AgencyName { get; set; }
         public string MissionType { get; set; }
     }
 
-    public class MainData
+    public class Mission
     {
-        public string CountryCode { get; set; }
-        public string MissionDescription { get; set; }
+        public int RelativeTime { get; set; }
+        public int Duration { get; set; }
+        public string Description { get; set; }
+        public string Name { get; set; }
+        public string WikiURL { get; set; }
+        public string URLs { get; set; }
+    }
+
+    public class Launch
+    {
         public LaunchStatus Status { get; set; }
         public string HoldReadon { get; set; }
         public string FailReason { get; set; }
+        public string InfoURLs { get; set; }
+        public string VideoURLs { get; set; }
     }
+
+    public class Rocket
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string ImageURL { get; set; }
+        public string InfoURL { get; set; }
+        public string WikiURL { get; set; }
+    }
+
+    public class Location
+    {
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string wikiURL { get; set; }
+        public string infoURL { get; set; }
+    }
+
+    public class PayLoad
+    {
+        public string Name { get; set; }
+        public string CountryCode { get; set; }
+        public string Descryption { get; set; }
+        public string Weight { get; set; }
+        public string Dimensions { get; set; }
+        public int Total { get; set; }
+        public string MissionsID { get; set; }
+    }
+
+
+
 }
