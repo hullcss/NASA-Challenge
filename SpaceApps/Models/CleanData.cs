123e947f99b2b90ee2c56472a309a67e5595e3de
Unknown <alex.denys.lovett@gmail.com>
2018-10-20 15:00:55 +0100

    added main data

--- SpaceApps/Models/CleanData.cs
+++ SpaceApps/Models/CleanData.cs
@@ -5,7 +5,25 @@ using System.Threading.Tasks;
 
 namespace SpaceApps.Models.CleanData
 {
+    public enum LaunchStatus
+    {
+        StatusNotFound,
+        Green,
+        Red,
+        Success,
+        Fail,
+    };
     public class CleanData
     {
+
+    }
+
+    public class MainData
+    {
+        public string CountryCode { get; set; }
+        public string MissionDescription { get; set; }
+        public LaunchStatus Status { get; set; }
+        public string HoldReadon { get; set; }
+        public string FailReason { get; set; }
     }
 }
