0e601bb14b52eb28c6b50d45cd017b03d300330b
ItDidntCatchFire <deaddanster@googlemail.com>
2018-10-20 10:27:36 +0100

    Broke everything

---
+++ SpaceApps/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SpaceApps.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
