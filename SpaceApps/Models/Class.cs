43bfc0b2fc08395821d07053cb21facf2ed9e017
Unknown <alex.denys.lovett@gmail.com>
2018-10-20 11:20:18 +0100

    added class

---
+++ SpaceApps/Models/Class.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace SpaceApps.Models.RawData
+{
+    public class Location
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public string countrycode {get; set;}
+        public string wikiURL {get; set;}
+        public string infoURL {get; set;}
+        public string[] infoURLs {get; set;}
+        public string changed {get; set;}
+    }
+
+    //public class 
+}
