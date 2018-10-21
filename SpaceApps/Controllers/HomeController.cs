c974a0d29e2149f528306bb00a1355cf941fb46b
ItDidntCatchFire <deaddanster@googlemail.com>
2018-10-21 14:08:24 +0100

    Commmitng

--- SpaceApps/Controllers/HomeController.cs
+++ SpaceApps/Controllers/HomeController.cs
@@ -26,9 +26,9 @@ namespace SpaceApps.Controllers
         public async Task<IActionResult> Launch(int launchId)
         {
             MainLaunch launchModel;
-            using(HttpClient client = new HttpClient())
+            using (HttpClient client = new HttpClient())
             {
-                client.BaseAddress =new Uri("https://localhost:5001");
+                client.BaseAddress = new Uri("https://localhost:5001");
                 var result = await client.GetAsync($"launch/grab/?id={launchId}");
                 var resultString = await result.Content.ReadAsStringAsync();
                 launchModel = JsonConvert.DeserializeObject<MainLaunch>(resultString);
